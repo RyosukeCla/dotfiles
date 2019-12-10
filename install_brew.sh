a63e96a8017f5ef90e0514aab0f99248720217cf
RyosukeCla <ryosukeclarinet@gmail.com>
2019-12-10 19:41:35 +0900

    fix

--- install_brew.sh
+++ install_brew.sh
@@ -8,6 +8,7 @@ DOT_DIR="$HOME/.config/ryosuke-dotfiles"
 main() {
 	echo "run install_brew.sh ..."
   if hash brew 2>/dev/null; then
+    echo "brew has already installed"
   else
     # install homebrew
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
