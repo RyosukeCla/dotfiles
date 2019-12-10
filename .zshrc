091cd03b8b775440d6b0db0e1e8a55f92deac32e
RyosukeCla <ryosukeclarinet@gmail.com>
2019-12-10 13:19:23 +0900

    first commit

---
+++ .zshrc
@@ -0,0 +1,22 @@
+# bashprofile
+
+export NVM_DIR="$HOME/.nvm"
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+  [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
+
+export GO111MODULE="on"
+
+# zshprofile from here
+
+ZPREZTO_PATH=${HOME}/.zsh/prezto
+ZPREZTO_INIT_PATH=${ZPREZTO_PATH}/init.zsh
+if [[ -s "${ZPREZTO_INIT_PATH}" ]]; then
+  source "${ZPREZTO_INIT_PATH}"
+fi
+
+function select-history() {
+  BUFFER=$(history -n -r 1 | fzf --height 40% --reverse --no-sort --query "$LBUFFER" --prompt=" ❥ History > ")
+  CURSOR=$#BUFFER
+}
+zle -N select-history
+bindkey '^r' select-history
---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# @(#) install.sh  - install dotfiles
+#
+
+DOT_DIR="$HOME/.config/ryosuke-dotfiles"
+
+main() {
+	echo "run install.sh ..."
+	get_repo
+	cd ${DOT_DIR}
+	config_git
+	do_install
+}
+
+config_git() {
+	git config user.name RyosukeCla
+	git config user.email RyosukeCla@users.noreply.github.com
+  git config core.ignorecase false
+}
+
+get_repo() {
+	if [ ! -e ${DOT_DIR} ]; then
+		git clone https://github.com/RyosukeCla/dotfiles.git ${DOT_DIR}
+	else
+		echo "${DOT_DIR} already exists."
+	fi
+}
+
+do_install() {
+	cd ${DOT_DIR}
+	TARGETS=$(git ls-files ".*" | awk -F'/' '{print $1}' | uniq)
+
+	for file in ${TARGETS[@]}
+	do
+		ln -s ${DOT_DIR}/${file} ${HOME}/${file}
+		if [ $? -eq 0 ]; then
+			echo "symlink craeted: ${file}"
+		fi
+	done
+}
+
+main
