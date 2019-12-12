c6a50c44fb5d75e8bd38bf480c8eccd4f123639e
RyosukeCla <RyosukeCla@users.noreply.github.com>
2019-12-12 21:00:53 +0900

    fix

--- install_go_mac.sh
+++ install_go_mac.sh
@@ -7,3 +7,7 @@ mkdir -p $TMP_DIR
 curl -L https://dl.google.com/go/go1.13.5.darwin-amd64.tar.gz -o $TMP_DIR/go1.13.5.darwin-amd64.tar.gz
 cd $TMP_DIR
 tar zxvf $TMP_DIR/go1.13.5.darwin-amd64.tar.gz
+
+ln -s ${TMP_DIR}/go/bin/go /usr/local/bin/go
+
+echo "symlink ${TMP_DIR}/go/bin/go to /usr/local/bin/go"
