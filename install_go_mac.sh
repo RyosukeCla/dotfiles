eb724d6cc6b0b4c31560c8df913299e59e0ea3c6
Ryosuke Suzuki <ryosukeclarinet@gmail.com>
2022-01-27 14:20:56 +0900

    Update install_go_mac.sh

--- install_go_mac.sh
+++ install_go_mac.sh
@@ -4,9 +4,9 @@ DOT_DIR="$HOME/.config/ryosuke-dotfiles"
 TMP_DIR=${DOT_DIR}/tmp
 
 mkdir -p $TMP_DIR
-curl -L https://dl.google.com/go/go1.13.5.darwin-amd64.tar.gz -o $TMP_DIR/go1.13.5.darwin-amd64.tar.gz
+curl -L https://go.dev/dl/go1.17.6.darwin-arm64.tar.gz -o $TMP_DIR/go1.17.6.darwin-arm64.tar.gz
 cd $TMP_DIR
-tar zxvf $TMP_DIR/go1.13.5.darwin-amd64.tar.gz
+tar zxvf $TMP_DIR/go1.17.6.darwin-arm64.tar.gz
 
 ln -s ${TMP_DIR}/go/bin/go /usr/local/bin/go
 
