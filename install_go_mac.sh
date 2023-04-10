9b657b8341a6191d26ccda71d5ae3281bbf0b699
Ryosuke Suzuki <ryosukeclarinet@gmail.com>
2023-04-10 15:12:09 +0900

    Update install_go_mac.sh

--- install_go_mac.sh
+++ install_go_mac.sh
@@ -6,6 +6,6 @@ TMP_DIR=${DOT_DIR}/tmp
 mkdir -p $TMP_DIR
 curl -L https://go.dev/dl/go1.20.3.darwin-arm64.tar.gz -o $TMP_DIR/go1.20.3.darwin-arm64.tar.gz
 cd $TMP_DIR
-tar zxvf $TMP_DIR/go1.18.darwin-arm64.tar.gz
+tar zxvf $TMP_DIR/go1.20.3.darwin-arm64.tar.gz
 
 echo "go is installed to ${TMP_DIR}/go/bin/go"
