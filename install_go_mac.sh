fa154716a3784359c088819e3ada2f1849380dad
Ryosuke Suzuki <ryosukeclarinet@gmail.com>
2025-04-25 17:18:00 +0900

    Update install_go_mac.sh

--- install_go_mac.sh
+++ install_go_mac.sh
@@ -6,6 +6,9 @@ TMP_DIR=${DOT_DIR}/tmp
 mkdir -p $TMP_DIR
 curl -L https://go.dev/dl/go1.24.2.darwin-arm64.tar.gz -o $TMP_DIR/go1.24.2.darwin-arm64.tar.gz
 cd $TMP_DIR
+if [ -d $TMP_DIR/go ]; then rm -rf $TMP_DIR/go; fi
 tar zxvf $TMP_DIR/go1.24.2.darwin-arm64.tar.gz
 
 echo "go is installed to ${TMP_DIR}/go/bin/go"
+echo "append this line to your zshrc"
+echo "  export GOPATH=\"\$HOME/go\""
